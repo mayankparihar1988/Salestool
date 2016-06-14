ba6b480184b9e98eae877293f7aa1daf2288e59c
Mayank Parihar <mayankparihar0008@gmail.com>
2016-06-14 20:34:06 +0200

    Test one first commit

---
+++ GitExample/src/sample/java/git/SalesData.java
@@ -0,0 +1,11 @@
+package sample.java.git;
+
+public class SalesData {
+	public int[] data = {0,4,42};
+	
+	public void display()
+	{
+		System.out.println("Hello i am sales ");
+	}
+
+}
---
+++ GitExample/src/sample/java/git/main.java
@@ -0,0 +1,19 @@
+package sample.java.git;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		 SalesData data = new SalesData();
+		 displayGreeting();
+		 data.display();
+	
+	}
+
+	public static void displayGreeting()
+	{
+		System.out.println("Hello Sales Person");
+		System.out.println("This Application Shows Sales Data");
+	}
+}
